1df70d0ea9c755e484e320490e8d186d4929d57e
Nithin John Cherian <nithinjohn0728@gmail.com>
2022-11-23 20:11:31 +0000

    modiifed app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const namesArray = ["Alice","Bob","Charlie"]
+  const int = Math.floor(Math.random() * 3)
+
+  const returnNumber = ()=>{
+    const numArray = [1,2,3]
+    const result = Math.floor(Math.random()*3)
+    return numArray[result]
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+          <p>Hello {namesArray[int]}</p>
+          <p>Hi {returnNumber()}</p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
