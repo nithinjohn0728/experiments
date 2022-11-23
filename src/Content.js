c5c4d46e4282117d7a95051971d9c38ef27c6776
Nithin John Cherian <nithinjohn0728@gmail.com>
2022-11-23 21:51:58 +0000

    click functions

--- src/Content.js
+++ src/Content.js
@@ -9,10 +9,31 @@ const Content = () => {
     const result = Math.floor(Math.random() * 3);
     return numArray[result];
   };
+
+  const handleClick = () => {
+    alert("clicked");
+  };
+
+  const handleClick2 = (name) => {
+    alert(`${name} is clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    alert(`${e} is clicked`);
+  };
   return (
     <div>
       <p>Hello {namesArray[int]}</p>
       <p>Hi {returnNumber()}</p>
+      <button onClick={handleClick}>click me</button>
+      <button onClick={(e) => handleClick(e)}>click event</button>
+      <button
+        onClick={() => {
+          handleClick2("get");
+        }}
+      >
+        click with name
+      </button>
     </div>
   );
 };
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const today = new Date();
   return (
     <div>
       <p>@2022 all rights reserved</p>
+      <p>{today.toLocaleString()}</p>
     </div>
   );
 };
